3f5bfbd0b121bc939e0ab7f0c22166e40eb2c869
EC2 Default User <ec2-user@ip-172-31-2-124.ap-northeast-1.compute.internal>
2021-06-30 06:46:02 +0000

    再スタート

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,9 @@
+
+$(window).on("load", function(){
+    $("#menu").on("click", function(){
+        $(".navi").show();
+    $("#closed").on("click", function(){
+        $(".navi").hide();
+    });
+    });
+});
