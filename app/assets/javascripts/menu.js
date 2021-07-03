b325a3840dcb29d0c88d5b753c08419415ca01eb
EC2 Default User <ec2-user@ip-172-31-2-124.ap-northeast-1.compute.internal>
2021-07-03 05:37:26 +0000

    一休み

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,10 +1,21 @@
+var menu = document.getElementById("menu");
+var back = document.getElementById("back");
+var nav = document.getElementById("nav");
 
-$(window).on("load", function(){
-    $("#nav").on("click", function(){
-      $(".nav").removeClass("nav");
-      $("#nav div").addClass("nav");
-      $(this).show();
-      $(".header-contents").hide();
-      $(".header-contents").show();
-    });
+
+menu.addEventListener("click", () => {
+ if (nav.className === "navi") {
+     nav.classList.add("open-menu");
+     back.classList.add("open");
+     menu.textContent = "閉じる";
+ }else {nav.classList.remove("open-menu");
+     back.classList.remove("open");
+        menu.textContent = "menu";
+       }
+});
+ 
+back.addEventListener("click", () => {
+    back.classList.remove("open");
+    nav.classList.remove("open-menu");
+    menu.textContent = "menu";
 });
