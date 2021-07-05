76bf0b8b89cf349aaebc19a7dbf44f62fa9b3e9b
EC2 Default User <ec2-user@ip-172-31-2-124.ap-northeast-1.compute.internal>
2021-07-05 01:29:14 +0000

    編集終了

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,21 +1,9 @@
-var menu = document.getElementById("menu");
-var back = document.getElementById("back");
-var nav = document.getElementById("nav");
-
-
-menu.addEventListener("click", () => {
- if (nav.className === "navi") {
-     nav.classList.add("open-menu");
-     back.classList.add("open");
-     menu.textContent = "閉じる";
- }else {nav.classList.remove("open-menu");
-     back.classList.remove("open");
-        menu.textContent = "menu";
-       }
-});
- 
-back.addEventListener("click", () => {
-    back.classList.remove("open");
-    nav.classList.remove("open-menu");
-    menu.textContent = "menu";
+$(window).on("load",function(){
+$("button").on("click",function(){
+      $("#nav ul li").show();
+      
+      $(this).on("click",function(){
+      $("#nav ul li").hide(); 
+   });
+   });
 });
